c08e1266a60a322ab9057c05a2e95ba7aeb677a0
Avinash Reddy <avinashreddy.idio@gmail.com>
2026-02-06 06:44:54 -0500

    http to https

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react"
 import { phoneNumberClient } from "better-auth/client/plugins"
 
 export const authClient = createAuthClient({
-    baseURL: "http://bfactory.app",
+    baseURL: "https://bfactory.app",
     plugins: [
         phoneNumberClient()
     ]
